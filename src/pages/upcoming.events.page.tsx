7f6f1b523cf705e4c717a2ee4f09d14c964d800e
LoneStamp <nissandiezonencaboagripoz9@gmail.com>
2025-12-26 18:16:24 +0800

    error TS6133,

--- src/pages/upcoming.events.page.tsx
+++ src/pages/upcoming.events.page.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const UpcomingEventsPage = () => (
   <div className="container mx-auto px-4 py-16">
     <h1 className="text-3xl font-bold text-center mb-8">Upcoming Events</h1>
